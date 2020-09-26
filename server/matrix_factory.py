def1e3f8cafd9bed5d9aad4d02df7e2c5ca38752
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2020-09-26 21:00:44 +0200

    reduce options.brightness

--- server/matrix_factory.py
+++ server/matrix_factory.py
@@ -29,6 +29,7 @@ def matrix_factory(width):
     options.gpio_slowdown = 0
     options.pwm_lsb_nanoseconds = 100
     options.pwm_dither_bits = 1
+    options.brightness = 35
     options.pixel_mapper_config= "Rotate:90"
     matrix = RGBMatrix(options=options)
     return matrix
