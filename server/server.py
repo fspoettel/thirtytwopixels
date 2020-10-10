af66eb577009191951aeb3cbb694a146437afdf4
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2020-10-10 15:01:26 +0200

    Force clear after timeout when player shuts down w/o notice

--- server/server.py
+++ server/server.py
@@ -2,14 +2,22 @@ import io
 import os
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 import syslog
+from threading import Timer
 import time
 from PIL import Image
 import zmq
 
 IS_PRODUCTION = os.uname()[4][:3] == "arm"
+# shutting down/sleeping OS does not invoke `ncmpcpp.execute_on_player_state_change`
+# we want a timeout that ensures that our matrix is cleared
+# after a certain amount of time w/o a song or state change
+MATRIX_INACTIVITY_TIMEOUT = 4 * 60 * 60
 
 
 class MockMatrix:
+    def Clear(self):
+        print("Clearing matrix")
+
     def SetImage(self, im):
         byte_io = io.BytesIO()
         im.save(byte_io, format="PPM")
@@ -26,18 +34,34 @@ def matrix_factory(width):
     options.gpio_slowdown = 0
     options.pwm_lsb_nanoseconds = 100
     options.pwm_dither_bits = 1
-    panel = RGBMatrix(options=options)
-    return panel
+    matrix = RGBMatrix(options=options)
+    return matrix
 
 
 class Panel:
     def __init__(self):
+        self.matrix = matrix_factory(32) if IS_PRODUCTION else MockMatrix()
         self.prev_byte_io = None
-        self.panel = matrix_factory(32) if IS_PRODUCTION else MockMatrix()
+        self.timeout = None
 
     def clear(self):
         self.prev_byte_io = None
-        self.panel.Clear()
+        self.stop_inactivity_timer()
+        self.matrix.Clear()
+
+    def clear_if_unchanged(self, current_byte_io):
+        if current_byte_io == self.prev_byte_io:
+            self.clear()
+
+    def stop_inactivity_timer(self):
+        if self.timeout:
+            self.timeout.cancel()
+            self.timeout = None
+
+    def start_inactivity_timer(self, byte_io):
+        self.stop_inactivity_timer()
+        self.timeout = Timer(MATRIX_INACTIVITY_TIMEOUT, self.clear_if_unchanged, [byte_io])
+        self.timeout.start()
 
     def draw(self, message):
         byte_io = io.BytesIO(message)
@@ -45,7 +69,8 @@ class Panel:
         if not compare_bytes(self.prev_byte_io, byte_io):
             self.prev_byte_io = byte_io
             im = Image.open(byte_io)
-            self.panel.SetImage(im.convert("RGB"))
+            self.matrix.SetImage(im.convert("RGB"))
+            self.start_inactivity_timer(byte_io)
 
 
 def get_bytes(byte_io):
